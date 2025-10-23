eee8bccacfbd66f0d4e38dcaa002e2176a02b839
Kamil Wasiak <kamilwasiak1407@gmail.com>
2025-10-23 20:30:31 +0200

    Variables in javascript exercise completed

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,35 @@
-console.log("Hello World!")
+let firstName = "John";
+let surname = "Doe";
+console.log(firstName + " " + surname)
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23 + 97);
+console.log(23 + 97 + 12 + 42 + 93 + 174);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 25;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(percentage)
+
